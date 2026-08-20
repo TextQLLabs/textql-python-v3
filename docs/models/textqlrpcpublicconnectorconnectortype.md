a2297e8ef3349850058ff94dee94236d29230a41
Rodney Shen <114166609+rodnnnney@users.noreply.github.com>
2026-08-19 23:53:33 -0400

    chore: 🐝 Update SDK - Generate 1.1.2 (#38)
    * ## Python SDK Changes:
    * `textql.chats.attach_app()`:  `response.cell.union(ontology_query_cell).ontology_query_cell.auth_connector_type.enum(ana_billing)` **Added**
    * `textql.chats.attach_dashboard()`:  `response.cell.union(ontology_query_cell).ontology_query_cell.auth_connector_type.enum(ana_billing)` **Added**
    * `textql.chats.attach_dataset()`:  `response.cell.union(ontology_query_cell).ontology_query_cell.auth_connector_type.enum(ana_billing)` **Added**
    * `textql.chats.check_streamlit_health()`:  `response.cell.union(sql_cell).sql_cell.auth_connector_type.enum(ana_billing)` **Added**
    * `textql.chats.get_history()`:  `response.cells[].union(sql_cell).sql_cell.auth_connector_type.enum(ana_billing)` **Added**
    * `textql.chats.poll_events()`:  `response.events[].union(cell).cell.union(sql_cell).sql_cell.auth_connector_type.enum(ana_billing)` **Added**
    * `textql.chats.query_one_shot()`:  `response.cells[].union(ontology_query_cell).ontology_query_cell.auth_connector_type.enum(ana_billing)` **Added**
    * `textql.chats.run()`:  `response.cells[].union(sql_cell).sql_cell.auth_connector_type.enum(ana_billing)` **Added**
    * `textql.connectors.create()`: 
      *  `request.config.union(postgres).connector_type.enum(ana_billing)` **Added**
      *  `response.connector_type.enum(ana_billing)` **Added**
    * `textql.connectors.duplicate_connector()`:  `response.connector.union(aurora_metadata).connector_type.enum(ana_billing)` **Added**
    * `textql.connectors.get()`:  `response.connector.union(redshift_metadata).connector_type.enum(ana_billing)` **Added**
    * `textql.connectors.get_connectors()`:  `response.connectors[].union(firebolt_metadata).connector_type.enum(ana_billing)` **Added**
    * `textql.connectors.test()`: 
      *  `request.config.union(azure_synapse).connector_type.enum(ana_billing)` **Added**
    * `textql.connectors.update()`: 
      *  `request.config.union(azure_synapse).connector_type.enum(ana_billing)` **Added**
      *  `response.connector.union(trino_metadata).connector_type.enum(ana_billing)` **Added**
    * `textql.observability.get_check_record_fix()`:  `response.fix_patch_cell.union(ontology_query_cell).ontology_query_cell.auth_connector_type.enum(ana_billing)` **Added**
    * `textql.observability.get_thread_warnings()`:  `response.warnings_by_chat.Map<textql.rpc.public.observe.ThreadWarningList>.warnings[].fix_patch_cell.union(ontology_query_cell).ontology_query_cell.auth_connector_type.enum(ana_billing)` **Added**
    
    * empty commit to trigger [run-tests] workflow
    
    ---------
    
    Co-authored-by: speakeasybot <bot@speakeasyapi.dev>
    Co-authored-by: speakeasy-github[bot] <128539517+speakeasy-github[bot]@users.noreply.github.com>

--- docs/models/textqlrpcpublicconnectorconnectortype.md
+++ docs/models/textqlrpcpublicconnectorconnectortype.md
@@ -44,3 +44,4 @@ This is an open enum. Unrecognized values will not fail type checks.
 - `"FIREBOLT"`
 - `"KDB"`
 - `"MONGODB"`
+- `"ANA_BILLING"`
--- src/textql_sdk/models/textql_rpc_public_connector_connectortype.py
+++ src/textql_sdk/models/textql_rpc_public_connector_connectortype.py
@@ -37,6 +37,7 @@ TextqlRPCPublicConnectorConnectorType = Union[
         "FIREBOLT",
         "KDB",
         "MONGODB",
+        "ANA_BILLING",
     ],
     UnrecognizedStr,
 ]
