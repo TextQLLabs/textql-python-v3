82997c8c1250e56b9e004256ba84d4c4b18eff7c
Rodney Shen <114166609+rodnnnney@users.noreply.github.com>
2026-08-25 18:10:06 -0400

    chore: 🐝 Update SDK - Generate 1.1.7 (#43)
    * ci: regenerated with OpenAPI Doc , Speakeasy CLI 1.795.3
    
    * empty commit to trigger [run-tests] workflow
    
    ---------
    
    Co-authored-by: speakeasybot <bot@speakeasyapi.dev>
    Co-authored-by: speakeasy-github[bot] <128539517+speakeasy-github[bot]@users.noreply.github.com>

--- src/textql_sdk/_version.py
+++ src/textql_sdk/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "textql-sdk"
-__version__: str = "1.1.6"
+__version__: str = "1.1.7"
 __openapi_doc_version__: str = "1.0.0"
 __gen_version__: str = "2.932.9"
-__user_agent__: str = "speakeasy-sdk/python 1.1.6 2.932.9 1.0.0 textql-sdk"
+__user_agent__: str = "speakeasy-sdk/python 1.1.7 2.932.9 1.0.0 textql-sdk"
 
 try:
     if __package__ is not None:
